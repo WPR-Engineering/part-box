64081c8705279faa7ef0878300114f60d828d493
Eric Bartos <djbartos93@gmail.com>
2019-01-30 07:56:01 -0600

    hb now

--- config/initializers/netbox.rb
+++ config/initializers/netbox.rb
@@ -1,6 +1,6 @@
 if CONFIG[:NETBOX][:ENABLED]
   NetboxClientRuby.configure do |c|
-    c.netbox.auth.token = CONFIG[:NETBOX][:API_KEY]
+    c.netbox.auth.token = [:NETBOX][:API_KEY]
     c.netbox.api_base_url = [:NETBOX][:BASE_URL]
     config.faraday.logger = :logger
   end
