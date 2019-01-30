1cf029e819cc29d47d88dcb258e229a7ed6f8114
Eric Bartos <djbartos93@gmail.com>
2019-01-30 09:12:55 -0600

    why do you hate me?

--- config/initializers/netbox.rb
+++ config/initializers/netbox.rb
@@ -1,7 +1,7 @@
 if CONFIG[:NETBOX][:ENABLED]
   NetboxClientRuby.configure do |c|
-    c.netbox.auth.token = [:NETBOX][:API_KEY]
-    c.netbox.api_base_url = [:NETBOX][:BASE_URL]
-    config.faraday.logger = :logger
+    c.netbox.auth.token = "#{CONFIG[:NETBOX][:API_KEY]}"
+    c.netbox.api_base_url = "#{CONFIG[:NETBOX][:BASE_URL]}"
+    c.faraday.logger = :logger
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,12 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'devise'
+
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
